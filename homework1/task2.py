e65cb9e8c9f7b99f0e2e8e4327f9de7348ba04b2
PAVEL LESYUK <p.lesyuk@corp.vk.com>
2023-03-08 21:48:09 +0400

    Fix bugs and add exception handler

--- homework1/task2.py
+++ homework1/task2.py
@@ -1,12 +1,13 @@
 # Напишите функцию, которая при заданном целом числе n складывает цифры этого числа и выводит результат.
 
 def print_sum(n):
-    converted_n = str(n)
     sum_n = 0
-    for i in converted_n:
-        sum_n += int(i)
-    return sum_n
-
+    for i in n:
+        try:
+            sum_n += int(i)
+        except ValueError:
+            print('Unexpected argument')
+            exit()
+    print(sum_n)
 
 user_input = print_sum(input('Enter number: '))
-print(user_input)
