717de384ab552a69514dd56727ef1a69803e702e
Pavel Lesyuk <paul.lesyuk@gmail.com>
2023-04-30 17:37:53 +0400

    More solution examples of task3 and task4 added

--- homework1/task3.py
+++ homework1/task3.py
@@ -1,6 +1,11 @@
 # Найдите два ключа с самыми большими значениями в словаре.
 
+# Own solution
 my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20}
 sorted_keys = sorted(my_dict, key=my_dict.get, reverse=True)
 for i in range(2):
     print(sorted_keys[i])
+
+# Another way to get last 2 elements
+sorted_keys = sorted(my_dict, key=my_dict.get)[-2:]
+print(sorted_keys)
--- homework1/task4.py
+++ homework1/task4.py
@@ -2,30 +2,30 @@
 
 # Один из способов должен быть реализован самостоятельно, без использования встроенных возможностей списка.
 
-# First way
+# 1st solution
 some_list = [1, 4, 6, 9, 2, 3, 5]
 
 some_list.reverse()
 
-print(some_list) # [5, 3, 2, 9, 6, 4, 1]
+print(f'1st solution: {some_list}') # [5, 3, 2, 9, 6, 4, 1]
 
-# Second way
+# 2nd solution
 
 some_list = [1, 4, 6, 9, 2, 3, 5]
 
 some_list = list(reversed(some_list))
 
-print(some_list)
+print(f'2nd solution: {some_list}')
 
-# Third way
+# 3rd solution
 
 some_list = [1, 4, 6, 9, 2, 3, 5]
 
 some_list = some_list[::-1]
 
-print(some_list)
+print(f'3rd solution: {some_list}')
 
-# Fourth way (own implementation)
+# 4th solution (own implementation)
 
 some_list = [1, 4, 6, 9, 2, 3, 5]
 new_list = []
@@ -34,4 +34,42 @@ for n in some_list:
     i = -abs(some_list.index(n) + 1)
     new_list.insert(i, n)
 
-print(new_list)
+print(f'4th solution: {new_list}')
+
+# ---
+
+# 5th solution
+
+some_list = [1, 4, 6, 9, 2, 3, 5]
+new_list = []
+
+for i in some_list:
+    new_list.insert(0, i)
+
+print(f'5th solution: {new_list}')
+
+# 6th solution
+
+some_list = [1, 4, 6, 9, 2, 3, 5]
+
+for i in range(len(some_list)):
+    some_list.insert(i, some_list.pop())
+
+print(f'6th solution: {some_list}')
+
+# 7th solution (best, like python work)
+
+some_list = [1, 4, 6, 9, 2, 3, 5]
+
+# do not create a new list and iterate over half of the list
+for i in range(len(some_list) // 2):
+    some_list[i], some_list[len(some_list) - 1 - i] = some_list[len(some_list) - 1 - i], some_list[i]
+
+print(f'7th solution: {some_list}')
+
+# 8th solution
+
+some_list = [1, 4, 6, 9, 2, 3, 5]
+new_list = [some_list[i] for i in range(len(some_list) - 1, -1, -1)]
+
+print(f'8th solution: {new_list}')
