a949040e8530448b99979991cd096ae22f310899
Pavel Lesyuk <paul.lesyuk@gmail.com>
2023-05-04 15:28:20 +0400

    Some changes and descriptions in test_fixture_advanced.py

--- pytest_lection/tests/test_fixtures_advanced.py
+++ pytest_lection/tests/test_fixtures_advanced.py
@@ -3,12 +3,13 @@ import random
 import pytest
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def random_func():
     return random.randint(0, 100)
 
 
-@pytest.fixture(autouse=True, scope='function')
+# If we want to use some value in a fixture from another fixture, we need to pass the fixture to the fixture
+@pytest.fixture(autouse=True)
 def new_file(random_func):
     with open(f'test_file_{random_func}', 'w') as f:
         f.write('123123123123213213135142131203534536535413')
@@ -25,5 +26,7 @@ def test1():
     pass
 
 
-def test2():
+# If we want to use a value from autouse fixture, we need to explicitly pass fixture to a test
+def test2(new_file):
+    print(new_file.write('000000'))
     pass
